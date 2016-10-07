f77fbc550edebe91f68da1200a42cb8dc1037aa4
Marina1303 <pupina3@mail.ru>
2016-10-07 17:50:43 +0700

    Реализован сумматор

---
+++ calc.php
@@ -0,0 +1,17 @@
+<Html>
+<Head> 
+<Title> </Title>
+</Head>
+<Body>
+<form action="calc.php" method="GET">
+<input type="text" name="arg1" value="">
+<input type="text" name="arg2" value="">
+<input type="submit"  value="Cуммировать">
+</form>
+<?php
+if (isset($_GET['arg1']) && isset($_GET['arg2'])) {
+echo "Результат: ".intval($_GET['arg1'])+intval($_GET['arg2']);
+}
+?>
+</Body>
+</Html>
